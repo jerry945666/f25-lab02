bde556636ae2ee5fa9c951ec460cfb7fb80e56fd
Tianxiang Wang <tianxiaw@andrew.cmu.edu>
2025-09-04 02:38:38 +0800

    refactor(java): improve encapsulation and information hiding

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
@@ -1,13 +1,15 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Square implements Shape {
-    public double sideLen;
+    private final double sideLen;
 
     public Square(double sideLen) {
         this.sideLen = sideLen;
     }
-    
+
+    @Override
     public double getArea() {
         return sideLen * sideLen;
     }
+
 }
